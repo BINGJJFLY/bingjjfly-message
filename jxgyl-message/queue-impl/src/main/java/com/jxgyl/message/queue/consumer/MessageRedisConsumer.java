a8b22a375eb428b532190fea0eebcfde8fbd0490
wangjz <17091267797@163.com>
2020-02-07 14:57:07 +0800

    可靠消息队列rpoplpush

--- jxgyl-message/queue-impl/src/main/java/com/jxgyl/message/queue/consumer/MessageRedisConsumer.java
+++ jxgyl-message/queue-impl/src/main/java/com/jxgyl/message/queue/consumer/MessageRedisConsumer.java
@@ -2,6 +2,7 @@ package com.jxgyl.message.queue.consumer;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -24,7 +25,12 @@ public class MessageRedisConsumer implements MessageConsumer {
 
 	@Override
 	public List<Message> consumeEmail() {
-		Message[] emails = redisTemplate.boundListOps(RedisMQ.EMAIL_QUEUE).rightPop();
+		// 这里可以使用阻塞队列
+		Message[] emails = redisTemplate.boundListOps(RedisMQ.EMAIL_QUEUE).rightPop(0, TimeUnit.MILLISECONDS);
+		// 应该添加日志记录消息已被取出
+		// 这里还是有问题，消息被取出后由于网络问题或者是应用服务器问题，导致消息未被正常消费，消息丢失，使用RPOPLPUSH
+		// 加入一个处理列表
+//		redisTemplate.opsForList().rightPopAndLeftPush(RedisMQ.EMAIL_QUEUE, "temp_email_queue", 0, TimeUnit.MILLISECONDS);
 		if (emails != null) {
 			return Arrays.asList(emails);
 		}
